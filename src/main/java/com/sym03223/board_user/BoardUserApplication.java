a9cdc4d3ed27b118195a04daaab40eec062f956c
sym03223 <sym03223@gmail.com>
2023-02-23 00:52:02 +0900

    ëfirst

---
+++ src/main/java/com/sym03223/board_user/BoardUserApplication.java
@@ -0,0 +1,13 @@
+package com.sym03223.board_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sym03223/board_user/BoardUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sym03223.board_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
